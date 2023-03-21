9f90f11ca1cbc60d8538148df077d382545fb757
Monica Nyaguthii Macharia <monicamacharia7@gmail.com>
2023-03-21 14:46:04 +0300

    logo & link: propeller

--- Development/Code/Blackbird Internship/blackbird/src/App.js
+++ Development/Code/Blackbird Internship/blackbird/src/App.js
@@ -1,17 +1,17 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="https://www.propelleraero.com/wp-content/uploads/2021/05/Vector.svg" className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.propelleraero.com/dirtmate/"
           target="_blank"
           rel="noopener noreferrer"
         >
